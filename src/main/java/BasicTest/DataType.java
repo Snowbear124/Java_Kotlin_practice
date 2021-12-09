47d4e8c6477cb102b4efa096f4958c4695728bdd
Snowbear124 <68908357+Snowbear124@users.noreply.github.com>
2021-12-09 12:29:01 +0800

    2 - Java data type practice.

---
+++ src/main/java/BasicTest/DataType.java
@@ -0,0 +1,21 @@
+package BasicTest;
+
+public class DataType {
+    public static void main(String[] args) {
+        int i = 12;
+        char c = 'A';
+        float f = 1.23f;
+        double d = 3.21f;
+        boolean bo = false;
+
+        Integer i2 = 12;
+        Character c2 = 'A';
+        Float f2 = 1.23f;
+        Boolean bo2 = false;
+        String s = "abc";
+
+        i2.floatValue();
+
+        System.out.println(i2);
+    }
+}
