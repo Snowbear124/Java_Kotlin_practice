6dc0742bb7d79c8126a4f3f13cd447fc110d63f3
Snowbear124 <68908357+Snowbear124@users.noreply.github.com>
2021-12-23 19:37:33 +0800

    3 - Java property practice.

---
+++ src/main/java/BasicTest/Add.java
@@ -0,0 +1,23 @@
+package BasicTest;
+
+public class Add {
+    int n1;
+    int n2;
+
+    public Add() {
+
+    }
+
+    public Add(int n1, int n2) {
+        this.n1 = n1;   // this = class Add()
+        this.n2 = n2;
+    }
+
+    public int add2Num() {
+        int num = n1 + n2;
+        return num;
+    }
+}
+
+
+
--- src/main/java/BasicTest/HelloJava.java
+++ src/main/java/BasicTest/HelloJava.java
@@ -2,7 +2,17 @@ package BasicTest;
 
 public class HelloJava {
     public static void main(String[] args) {
-        Call p = new Call();    // 只能呼叫class，不能使用class+function，kotlin可以
-        p.hello();
+//        Call p = new Call();    // 只能呼叫class，不能使用class+function，kotlin可以
+//        p.hello();
+
+//        Add number = new Add();
+//        number.n1 = 3;
+//        number.n2 = 4;
+//        int sum = number.add2Num();
+//        System.out.println(sum);
+
+        Add number = new Add(3, 2);
+        int sum = number.add2Num();
+        System.out.println(sum);
     }
 }
